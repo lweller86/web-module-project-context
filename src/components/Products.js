0703c7e20dc6121161a11e0080d4e6f0a6e4c36c
lweller86 <112785242+lweller86@users.noreply.github.com>
2023-02-15 07:29:09 -0500

    finished with Context API part of the app

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Components
 import Product from './Product';
+import ProductContext from './Contexts/ProductContext';
 
 const Products = props => {
+	const { products } = useContext(ProductContext)
+
+	
 	return (
 		<div className="products-container">
-			{props.products.map(product => (
+			{products.map(product => (
 				<Product
 					key={product.id}
 					product={product}
-					addItem={props.addItem}
+
 				/>
 			))}
 		</div>
